c41f7a7711b5e0cf430271e231f2eb85256e50cb
vanhien1199-art <vanhien1199@gmail.com>
2025-11-26 16:58:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'), // Dấu chấm trỏ vào src giúp import ngắn gọn
     },
   },
-  // Cloudflare Pages tương thích tốt nhất với output directory là 'dist'
   build: {
     outDir: 'dist',
   }
