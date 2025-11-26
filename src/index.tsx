20e19cd43b3d9a97c51f6ef29d6b04d4624d74ce
vanhien1199-art <vanhien1199@gmail.com>
2025-11-26 16:49:25 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <-- BẮT BUỘC PHẢI CÓ DÒNG NÀY ĐỂ NHẬN TAILWIND
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
