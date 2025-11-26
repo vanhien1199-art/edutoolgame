2a123c4279403bb3632197683f3dacd18111b96b
vanhien1199-art <vanhien1199@gmail.com>
2025-11-26 23:03:10 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // <-- BẮT BUỘC PHẢI CÓ DÒNG NÀY ĐỂ NHẬN TAILWIND
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
